d33339c2228f1d55ef077f19af17ffe6fe7c8090
counter-cowboy <79208562003@ya.ru>
2023-11-23 11:10:02 +0300

    docs: 425 extractNameFromTemplate

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -421,8 +421,8 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value.slice(value.indexOf(' ') + 1, value.length - 1);
 }
 
 /**
