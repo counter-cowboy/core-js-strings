2e578586887b6bf808feb55c6db963735e0c727f
counter-cowboy <79208562003@ya.ru>
2023-11-23 10:17:50 +0300

    docs: str 353 findLongestWord

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -343,8 +343,17 @@ function isPalindrome(str) {
  *   findLongestWord('A long and winding road') => 'winding'
  *   findLongestWord('No words here') => 'words'
  */
-function findLongestWord(/* sentence */) {
-  throw new Error('Not implemented');
+function findLongestWord(sentence) {
+  const sentArray = sentence.split(' ');
+  let theLongestWord = sentArray[0];
+  let theLongest = theLongestWord.length;
+  for (let i = 0; i < sentArray.length; i += 1) {
+    if (sentArray[i].length > theLongest) {
+      theLongestWord = sentArray[i];
+      theLongest = sentArray[i].length;
+    }
+  }
+  return theLongestWord;
 }
 
 /**
